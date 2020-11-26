602da0b03b3c185c8ea9211b41c97208371d238a
Robin <bzhou830@163.com>
2020-11-26 17:25:28 +0800

    refine dht11.

--- pi/dht11.py
+++ pi/dht11.py
@@ -9,16 +9,14 @@ class DHT:
     # GPIO口定义
     def __init__(self):
         self.sensor = Adafruit_DHT.DHT11
+        self.hum = 0.0
+        self.temp = 0.0
 
     def get_temp_hum(self):
-        while True:
-            humidity, temperature = Adafruit_DHT.read_retry(self.sensor, makerobo_pin)
-            if humidity is not None and temperature is not None:
-                print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-                break
-            else:
-                print('Failed to get reading. Try again!')
-        return humidity, temperature
+        self.hum, self.temp = Adafruit_DHT.read_retry(self.sensor, makerobo_pin)
+        if self.hum is not None and self.temp is not None:
+            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(self.temp, self.hum))
+        return self.hum, self.temp
 
     def destroy(self):
         GPIO.cleanup()  # 释放资源
